6c202f8ad391397ecf92a5c55f6e4d62399f072b
Jonathan Kim <jonnykimbk@hotmail.com>
2023-04-06 15:16:08 -0300

    analytics

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Resume from "./components/Resume/Resume";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <div>
       <Resume />
+      <Analytics />
     </div>
   );
 }
