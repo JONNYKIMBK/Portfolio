83d07147ce8499cb52bb4ccfd8f625d35004a175
Jonathan Kim <jonnykimbk@hotmail.com>
2023-05-04 11:23:42 -0300

    logos

--- portfolio/src/app/components/Resume/Resume.tsx
+++ portfolio/src/app/components/Resume/Resume.tsx
@@ -92,29 +92,109 @@ export default function Resume() {
                 <p>{lang === "es" ? es.summary.lang : en.summary.lang}</p>
 
                 <ul>
-                  <li key="Javascript">Javascript</li>
-                  <li key="Typescript">Typescript</li>
+                  <li key="Javascript">
+                    <img
+                      src="https://camo.githubusercontent.com/899faceac4ba793d1b47c461a572a228f852b16827a75a3c1744db1502d2b4ce/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f353936382f353936383239322e706e67"
+                      alt="Javascript"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    Javascript
+                  </li>
+                  <li key="Typescript">
+                    <img
+                      src="https://camo.githubusercontent.com/fc7b04de41a9e7c4f788a5311d10d3f14db021edfd1e91c1b526b7ebb3f06f5c/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f353936382f353936383338312e706e67"
+                      alt="Typescript"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    Typescript
+                  </li>
                 </ul>
 
                 <p>Backend:</p>
                 <ul>
-                  <li key="Node.js">Node.js</li>
-                  <li key="Express.js">Express.js</li>
+                  <li key="Node.js">
+                    <img
+                      src="https://camo.githubusercontent.com/c128a632c96ba308e5ede4c2f0ec513856f663c6794c27e1624beb8a88aa7309/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f3931392f3931393832352e706e67"
+                      alt="Node"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    Node.js
+                  </li>
+                  <li key="Express.js">
+                    <img
+                      src="https://camo.githubusercontent.com/5707f6e31f0340f0f98c21f65e5d7f2eeb50d1d64b9fec28220f8fd89b9ccd36/68747470733a2f2f77372e706e6777696e672e636f6d2f706e67732f3932352f3434372f706e672d7472616e73706172656e742d657870726573732d6a732d6e6f64652d6a732d6a6176617363726970742d6d6f6e676f64622d6e6f64652d6a732d746578742d74726164656d61726b2d6c6f676f2e706e67"
+                      alt="Express"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    Express.js
+                  </li>
                 </ul>
               </div>
               <div className={styles.first}>
                 <p>Frontend:</p>
                 <ul>
-                  <li key="React.js">React.js</li>
-                  <li key="Redux">Redux</li>
-                  <li key="HTML5">HTML5</li>
-                  <li key="CSS3">CSS3</li>
-                  <li key="Material UI">Material UI</li>
+                  <li key="React.js">
+                    <img
+                      src="https://camo.githubusercontent.com/171f89b0d975a02e03e851541df5b1a0cf4a87aec6092a9f842c9d0f2b9ccb4a/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f313132362f313132363031322e706e67"
+                      alt="React"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    React.js
+                  </li>
+                  <li key="Redux">
+                    <img
+                      src="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png"
+                      alt="Redux"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    Redux
+                  </li>
+                  <li key="HTML5">
+                    <img
+                      src="https://camo.githubusercontent.com/0d780585b41551e0d1aeafb0a7841acd91586bcf39d5b7f8eceb707175fcacfd/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f3137342f3137343835342e706e67"
+                      alt="HTML5"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    HTML5
+                  </li>
+                  <li key="CSS3">
+                    <img
+                      src="https://camo.githubusercontent.com/809a763f1c8f3497709ff0a974bfe7dd11be4dd7a29085645f8e98fbaa4a26e4/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f3733322f3733323139302e706e67"
+                      alt="CSS3"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    CSS3
+                  </li>
+                  <li key="Material UI">
+                    <img
+                      src="https://mui.com/static/logo.png"
+                      alt="Material UI"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    Material UI
+                  </li>
                 </ul>
                 <p>{lang === "es" ? es.summary.db : en.summary.db}</p>
 
                 <ul>
-                  <li key="PostgreSQL">PostgreSQL</li>
+                  <li key="PostgreSQL">
+                    <img
+                      src="https://camo.githubusercontent.com/cec0f4d96b259673ed3ace71c57ba5e1c7dd8dcab9d0f249f2cce368f314adee/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f353936382f353936383334322e706e67"
+                      alt="PostgreSQL"
+                      width="20"
+                      height="20"
+                    />{" "}
+                    PostgreSQL
+                  </li>
                 </ul>
               </div>
             </div>
